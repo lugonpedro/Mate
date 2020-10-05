52cf940b8ce40631ca80eaff5d476247012dba29
Pedro Motta <pedromotta20@gmail.com>
2020-10-05 19:58:47 -0300

    Busca por locais funcionando

--- mobile_passenger/src/pages/Route/index.js
+++ mobile_passenger/src/pages/Route/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, Alert } from 'react-native';
 import MapView from 'react-native-maps';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import MapViewDirections from 'react-native-maps-directions';
 
 import styles from './styles';
 
@@ -58,24 +59,24 @@ export default class Route extends React.Component {
                     />
 
                     <GooglePlacesAutocomplete
-                        placeholder="Local de Saida"
+                        placeholder="Onde Mora"
                         fetchDetails={true}
-                        onPress={() => { }}
+                        onPress={(data, details) => {
+                            console.log(data, details);
+                        }}
                         query={{
                             // available options: https://developers.google.com/places/web-service/autocomplete
-                            key: 'AIzaSyAGBbXF9Uoon0qFrlcJ8E7viGEUb93e1n4',
+                            key: 'AIzaSyC-0wZK4QIMROxSg93uIctMRf-p-keq9Ww',
                             language: 'pt-BR',
                         }}
                         textInputProps={{
                             autoCapitalize: "none",
                             autoCorrect: false
                         }}
-                        currentLocation={true}
-                        currentLocationLabel="Localizacao atual"
                         enablePoweredByContainer={false}
                         styles={{
                             container: {
-                                position: "absolute",
+                                position: 'absolute',
                                 width: '100%',
                                 top: 10,
                             },
@@ -101,11 +102,11 @@ export default class Route extends React.Component {
                                 elevation: 5,
                                 shadowColor: '#000',
                                 shadowOpacity: 0.1,
-                                shadowOffset: { x: 0, y: 0},
+                                shadowOffset: { x: 0, y: 0 },
                                 shadowRadius: 15,
                                 borderWidth: 1,
                                 borderColor: "#DDD",
-                                fontSize: 18,
+                                fontSize: 14,
                             },
                             listView: {
                                 borderWidth: 1,
@@ -115,12 +116,84 @@ export default class Route extends React.Component {
                                 elevation: 5,
                                 shadowColor: "#000",
                                 shadowOpacity: 0.1,
-                                shadowOffset: { x: 0, y: 0},
+                                shadowOffset: { x: 0, y: 0 },
                                 shadowRadius: 15,
                                 marginTop: 10,
                             },
                             description: {
-                                fontSize: 16,
+                                fontSize: 14,
+                            },
+                            row: {
+                                padding: 20,
+                                height: 58,
+                            }
+                        }}
+                    />
+
+                    <GooglePlacesAutocomplete
+                        placeholder="Onde Estuda"
+                        fetchDetails={true}
+                        onPress={(data, details) => {
+                            console.log(data, details);
+                        }}
+                        query={{
+                            // available options: https://developers.google.com/places/web-service/autocomplete
+                            key: 'AIzaSyC-0wZK4QIMROxSg93uIctMRf-p-keq9Ww',
+                            language: 'pt-BR',
+                        }}
+                        textInputProps={{
+                            autoCapitalize: "none",
+                            autoCorrect: false
+                        }}
+                        enablePoweredByContainer={false}
+                        styles={{
+                            container: {
+                                position: 'absolute',
+                                width: '100%',
+                                top: 70,
+                            },
+                            textInputContainer: {
+                                flex: 1,
+                                backgroundColor: 'transparent',
+                                height: 54,
+                                marginHorizontal: 20,
+                                borderTopWidth: 0,
+                                borderBottomWidth: 0,
+                            },
+                            textInput: {
+                                height: 54,
+                                margin: 0,
+                                borderRadius: 0,
+                                paddingTop: 0,
+                                paddingBottom: 0,
+                                paddingLeft: 20,
+                                paddingRight: 20,
+                                marginTop: 0,
+                                marginLeft: 0,
+                                marginRight: 0,
+                                elevation: 5,
+                                shadowColor: '#000',
+                                shadowOpacity: 0.1,
+                                shadowOffset: { x: 0, y: 0 },
+                                shadowRadius: 15,
+                                borderWidth: 1,
+                                borderColor: "#DDD",
+                                fontSize: 14,
+                            },
+                            listView: {
+                                borderWidth: 1,
+                                borderColor: '#DDD',
+                                backgroundColor: '#FFF',
+                                marginHorizontal: 20,
+                                elevation: 5,
+                                shadowColor: "#000",
+                                shadowOpacity: 0.1,
+                                shadowOffset: { x: 0, y: 0 },
+                                shadowRadius: 15,
+                                marginTop: 10,
+                            },
+                            description: {
+                                fontSize: 14,
                             },
                             row: {
                                 padding: 20,
