3ca40803199d598971d3712daf2169de7b989949
Luis Paulo <luispaulo.aganete@hotmail.com>
2020-10-30 15:29:33 -0300

    Listando dados do passageiro - erro

--- mobile_passenger/src/pages/Profile/index.js
+++ mobile_passenger/src/pages/Profile/index.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { View, TextInput, TouchableOpacity, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
@@ -6,8 +6,16 @@ import styles from './styles';
 
 import logo from '../../../assets/icon.png';
 
-export default function Profile() {
+import firebase from 'firebase';
+import 'firebase/firestore'
+
+
+export default function Profile(props) {
     const navigation = useNavigation();
+
+    const [passageiro, setPassageiro] = useState({});
+
+    const userDocumnt = await firebase.firestore().collection('passageiro').doc("2jVnEgkQL07FqGS7I01H").get()
 
     const [editable, setEditable] = useState(false);
 
@@ -20,7 +28,7 @@ export default function Profile() {
             <View style={styles.main}>
                 <TextInput placeholder={"Nome Completo"}
                     style={styles.input}
-                    defaultValue={""}
+                    defaultValue={passageiro.nome}
                     editable={editable}
                 />
 
@@ -28,21 +36,21 @@ export default function Profile() {
                     keyboardType={'numeric'}
                     maxLength={11}
                     style={styles.input}
-                    defaultValue={""}
+                    defaultValue={passageiro.telefone}
                     editable={editable} />
 
                 <TextInput placeholder={"Data de Nascimento"}
                     keyboardType={'numeric'}
                     maxLength={8}
                     style={styles.input}
-                    defaultValue={""}
+                    defaultValue={passageiro.dataNasc}
                     editable={editable} />
 
                 <TextInput placeholder={"CPF"}
                     keyboardType={'numeric'}
                     maxLength={11}
                     style={styles.input}
-                    defaultValue={""}
+                    defaultValue={passageiro.cpf}
                     editable={editable} />
 
                 {editable || (
