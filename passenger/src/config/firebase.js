f54a2d4bea3244e49374d1a9798d4b9eea3c675e
Pedro Lugon <pedromotta20@gmail.com>
2020-12-01 20:05:33 -0300

    fiz bobage

--- passenger/src/config/firebase.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyA21p6PaTpTr1XxkOh6TlxkySh10FUFCOw",
-    authDomain: "mate-93164.firebaseapp.com",
-    databaseURL: "https://mate-93164.firebaseio.com",
-    projectId: "mate-93164",
-    storageBucket: "mate-93164.appspot.com",
-    messagingSenderId: "296660954085",
-    appId: "1:296660954085:web:61a8cb444318262ad49d0c"
-  };
